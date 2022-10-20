e4837a45c79ff0faedbeb9cac70c2bd01bf729dc
Pradeep B - 3059 <pradibandi@gmail.com>
2022-10-20 10:57:46 +0530

    fdb

---
+++ 1.py
@@ -0,0 +1,60 @@
+# student_name='jules'
+#
+# marks = {'James': 90, 'jules':80, 'Arthur': 60 }
+#
+# for student in marks:
+#     if student == student_name:
+#         print(marks[student])
+#         continue
+#     else:
+#         print('No entry found with this name')
+from typing import List, Any
+
+import Oops
+
+
+# def display(a, b):
+#     print(a, b)
+# display(b=20, a=10)
+
+# def display(name,course = 'B.Tech'):
+#     print (name)
+#     print (course)
+#
+# display(name = 'abc', course='m.tech')
+# display(name = 'PQR')
+
+# def display(*courses):
+#
+#     for i in courses:
+#         print (i)
+# display ('B.tech','M.tech','MCA','MBA')
+
+# oddlist = [1,3,5,7]
+# evenlist = [2,4,6,8]
+#
+# pair = [(x,y) for x in oddlist for y in evenlist if x!=y]
+# print(pair)
+
+# lst = [(1,2,3,4,5,6,7)]
+# for i in lst:
+#     print(lst)
+# square = [i**9 for i in range (0,6)]
+# print (square)
+
+
+# def convert (string):
+#     li=list(string.split(" "))
+#     return li
+# str1 = "pradeep Bandi"
+# print(convert(str1))
+
+# file operation
+# fo=open("lst - dct.py","w")
+# fo.write("Hello")
+# fo.write("Welcome")
+# zdsf = ["\nThis \n","is\n","Files\n"]
+# fo.writelines(zdsf)
+# fo.close()
+
+
